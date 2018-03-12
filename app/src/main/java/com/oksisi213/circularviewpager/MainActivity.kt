f85818f44a32ee0ac5ce49d7463047357bebd9db
Charles <shok@maxst.com>
2018-03-12 11:21:46 +0900

    완성

--- app/src/main/java/com/oksisi213/circularviewpager/MainActivity.kt
+++ app/src/main/java/com/oksisi213/circularviewpager/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.TabLayout
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
@@ -27,11 +28,9 @@ class MainActivity : AppCompatActivity() {
 		tab_layout.addTab(tab_layout.newTab().setText("title 03"))
 		tab_layout.addTab(tab_layout.newTab().setText("title 04"))
 
-
 		view_pager.addOnPageChangeListener(pageChangeListener)
 		tab_layout.addOnTabSelectedListener(onTabSelectedListener)
-
-
+		view_pager.currentItem = 5000
 	}
 
 
@@ -48,7 +47,6 @@ class MainActivity : AppCompatActivity() {
 					text = "$i"
 				})
 			}
-
 			list
 		}
 
@@ -56,17 +54,29 @@ class MainActivity : AppCompatActivity() {
 
 		}
 
-		override fun isViewFromObject(view: View, obj: Any): Boolean = view == obj as View
+		override fun isViewFromObject(view: View, obj: Any): Boolean {
+			return view == obj as View
+		}
 
-		override fun getCount(): Int = 100
+		override fun getCount(): Int = Integer.MAX_VALUE
 		override fun instantiateItem(container: ViewGroup, position: Int): Any {
+			Log.e(TAG, "instantiateItem=$position")
 			context = container.context
-			return views[position]
+
+
+			val fakePosition = position % 5
+
+			if (container.indexOfChild(views[fakePosition]) != -1) {
+				container.removeView(views[fakePosition])
+			}
+			container.addView(views[fakePosition])
+
+			return views[fakePosition]
 		}
 
 		override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
-			super.destroyItem(container, position, view)
-			container.removeView(view as View)
+			Log.e(TAG, "destroyItem=$position")
+//			container.removeView(view as View)
 		}
 
 	}
@@ -79,8 +89,36 @@ class MainActivity : AppCompatActivity() {
 		}
 
 		override fun onTabSelected(tab: TabLayout.Tab?) {
+			Log.e(TAG, "onTabSelected = ${tab!!.position} page =${view_pager.currentItem}")
 			view_pager.removeOnPageChangeListener(pageChangeListener)
-			view_pager.setCurrentItem(tab!!.position, true)
+			tab_layout.removeOnTabSelectedListener(this)
+
+			val pagerPosition = view_pager.currentItem
+			val tabPosition = tab!!.position
+			val fakePagerPosition = view_pager.currentItem % 5
+
+			if (tabPosition % 5 == fakePagerPosition) {
+				Log.e(TAG, "do nothing")
+				return
+			}
+
+			val positionDiff = Math.abs(fakePagerPosition - tabPosition)
+			Log.e(TAG, "positionDiff = $positionDiff")
+
+
+			if (fakePagerPosition > tabPosition) {
+				for (i in 0 until positionDiff) {
+					Log.e(TAG, "decrease")
+					view_pager.currentItem--
+				}
+			} else if (tabPosition > fakePagerPosition) {
+				for (i in 0 until positionDiff) {
+					Log.e(TAG, "increase")
+					view_pager.currentItem++
+				}
+			}
+
+			tab_layout.addOnTabSelectedListener(this)
 			view_pager.addOnPageChangeListener(pageChangeListener)
 		}
 
@@ -89,10 +127,10 @@ class MainActivity : AppCompatActivity() {
 	val pageChangeListener: ViewPager.SimpleOnPageChangeListener = object : ViewPager.SimpleOnPageChangeListener() {
 		override fun onPageSelected(position: Int) {
 			super.onPageSelected(position)
+			Log.e(TAG, "onPageSelected = $position")
 			tab_layout.removeOnTabSelectedListener(onTabSelectedListener)
 			tab_layout.getTabAt(position % 5)?.select()
 			tab_layout.addOnTabSelectedListener(onTabSelectedListener)
-
 		}
 	}
 
